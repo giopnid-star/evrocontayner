331963ae4f00d10ad39ab283f9dda0a02654de89
giopnid-star <giopnid@gmail.com>
2026-02-08 17:31:58 +0500

    Change default port to 5500

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 const TO_EMAIL = process.env.TO_EMAIL || 'tigran.amarhanyan@gmail.com';
 const SMTP_HOST = process.env.SMTP_HOST;
 const SMTP_PORT = process.env.SMTP_PORT;
